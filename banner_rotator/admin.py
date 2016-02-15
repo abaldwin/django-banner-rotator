b5577204bf0c884fc3083d92a1106f3c705f95a0
Alex Baldwin <mail@abaldwin.com>
2016-02-14 19:18:41 -0800

    Rename module_name to model name in order to support Django 1.8

--- banner_rotator/admin.py
+++ banner_rotator/admin.py
@@ -65,7 +65,7 @@ class BannerAdmin(admin.ModelAdmin):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return update_wrapper(wrapper, view)
 
-        info = self.model._meta.app_label, self.model._meta.module_name
+        info = self.model._meta.app_label, self.model._meta.model_name
 
         urlpatterns = patterns('',
             url(r'^$', wrap(self.changelist_view), name='%s_%s_changelist' % info),
@@ -86,7 +86,7 @@ class BannerAdmin(admin.ModelAdmin):
 
         context = {
             'title': _('Log clicks'),
-            'module_name': capfirst(opts.verbose_name_plural),
+            'model_name': capfirst(opts.verbose_name_plural),
             'object': obj,
             'app_label': app_label,
             'log_clicks': Click.objects.filter(banner=obj).order_by('-datetime')
